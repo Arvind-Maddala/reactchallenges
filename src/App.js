ad9fd00ae6c8bff3b69ac44f2e3256da18f9f1d8
Arvind-Maddala <arvindprince10@gmail.com>
2023-05-30 01:06:47 +0530

    light & dark mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ParentComponent from "./components/parent/ParentComponent";
+import { useState } from "react";
 
 function App() {
+  const [ParentTheme, setParentTheme] = useState("light");
+  const handleParentChange = () => {
+    setParentTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={ParentTheme === "light" ? "parent" : "parentDark"}>
+      <ParentComponent
+        handleParentChangeFromApp={handleParentChange}
+        ParentTheme={ParentTheme}
+      />
     </div>
   );
 }
---
+++ src/components/child/ChildComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChildComponents = () => {
+  return (
+    <div>
+      <h1> The container color scheme is controlled by only CSS</h1>
+    </div>
+  );
+};
+
+export default ChildComponents;
